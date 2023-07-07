2abc1d8f35551ff091f5a9e0665d1b14bd3fb3a7
Ebrenes22 <e-j-bm@outlook.com>
2023-07-06 20:59:28 -0600

    Semana8

--- src/main/java/com/tienda/service/FirebaseStorageService.java
+++ src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -11,14 +11,14 @@ import org.springframework.stereotype.Service;
  *
  * @author Eliecer_Brenes
  */
+@Service
 public interface FirebaseStorageService {
-    
-    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta,Long id);
-    
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
     final String BucketName = "techshop-1b151.appspot.com";
     final String rutaSuperiorStorage = "techshop";
     final String rutaJsonFile = "firebase";
-    final String archivoJsonFile = "techshop-1b151-firebase-adminsdk-4q0ut-f25d2c57a4.json" ;
-    
-    
+    final String archivoJsonFile = "techshop-1b151-firebase-adminsdk-4q0ut-f25d2c57a4.json";
+
 }
---
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -0,0 +1,20 @@
+package com.tienda.service;
+
+import com.tienda.domain.Producto;
+import java.util.List;
+
+public interface ProductoService {
+
+    // Se obtiene un listado de productos en un List
+    public List<Producto> getProductos(boolean activos);
+
+    // Se obtiene un Producto a apartir del id de una producto
+    public Producto getProducto(Producto producto);
+
+    //Se inserta una nueva producto si el id del catetgoria esta vacio
+    // se actualiza una producto si el ide del cproducto no esta vacio
+    public void save(Producto producto);
+
+    //Se elimina la producto que tiene id por parametros
+    public void delete(Producto producto);
+}
